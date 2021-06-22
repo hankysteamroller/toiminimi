850b52477f0bafb47153faab409a7d3aa641103a
Hannu Lehtikangas <hannu.lehtikangas@gmail.com>
2021-06-22 19:41:36 +0300

    csv: Write utils

--- src/utils/csv.ts
+++ src/utils/csv.ts
@@ -3,8 +3,15 @@ import { Options } from 'csv-parse';
 import { Either, toError, tryCatch } from 'fp-ts/Either';
 
 import parse = require('csv-parse/lib/sync');
+import stringify = require('csv-stringify/lib/sync');
 
 export type CsvParseErr = Error;
 
 export const parseC = <T>(options: Options) => (a: string) =>
   tryCatch(() => parse(a, options), toError) as Either<CsvParseErr, T[]>;
+
+export const toCsv = (input: string[][]) =>
+  tryCatch(() => stringify(input, { delimiter: ';' }), toError) as Either<
+    Error,
+    string
+  >;
